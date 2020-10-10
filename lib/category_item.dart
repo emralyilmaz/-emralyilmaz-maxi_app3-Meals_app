1c514c97ee8c29694df1e3700ab18d40f000ca95
Emral Yılmaz <emralyilmaz@Emrals-MacBook-Air.local>
2020-10-10 22:52:49 +0300

    160. Navigating to a New Page

--- lib/category_item.dart
+++ lib/category_item.dart
@@ -1,3 +1,4 @@
+import 'package:app3_meals_app/category_meals_screen.dart';
 import 'package:flutter/material.dart';
 
 class CategoryItem extends StatelessWidget {
@@ -6,25 +7,35 @@ class CategoryItem extends StatelessWidget {
 
   CategoryItem(this.title, this.color);
 
+  void selectCategory(BuildContext ctx) {
+    Navigator.of(ctx).push(MaterialPageRoute(builder: (_) {
+      return CategoryMealsScreen();
+    }));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(15),
-      // const ile bu build method çalıştırıldığında, buradaki bu nesne yeniden oluşturulmayacaktır
-      // çünkü Flutter bunun sabit olduğunu, değiştirilemeyeceğini ve dolayısıyla eskisini kullanması gerektiğini algılar.
-      child: Text(
-        title,
-        style: TextStyle(
-            fontFamily: "RobotoCondensed",
-            fontSize: 18,
-            fontWeight: FontWeight.bold),
+    return InkWell(
+      onTap: () => selectCategory(context),
+      splashColor: Theme.of(context).primaryColor,
+      child: Container(
+        padding: const EdgeInsets.all(15),
+        // const ile bu build method çalıştırıldığında, buradaki bu nesne yeniden oluşturulmayacaktır
+        // çünkü Flutter bunun sabit olduğunu, değiştirilemeyeceğini ve dolayısıyla eskisini kullanması gerektiğini algılar.
+        child: Text(
+          title,
+          style: TextStyle(
+              fontFamily: "RobotoCondensed",
+              fontSize: 18,
+              fontWeight: FontWeight.bold),
+        ),
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                colors: [color.withOpacity(0.7), color],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight),
+            borderRadius: BorderRadius.circular(15)),
       ),
-      decoration: BoxDecoration(
-          gradient: LinearGradient(
-              colors: [color.withOpacity(0.7), color],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight),
-          borderRadius: BorderRadius.circular(15)),
     );
   }
 }
