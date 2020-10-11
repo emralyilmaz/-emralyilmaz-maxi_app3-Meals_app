102687df923cf081982ecd5e6822492730a63634
Emral Yılmaz <emralyilmaz@Emrals-MacBook-Air.local>
2020-10-11 19:46:31 +0300

    165. Selecting Meals for a Chosen Category

--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -13,13 +13,18 @@ class CategoryMealsScreen extends StatelessWidget {
         ModalRoute.of(context).settings.arguments as Map<String, String>;
     final categoryTitle = routeArgs["title"];
     final categoryId = routeArgs["id"];
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(categoryId);
+    }).toList();
     return Scaffold(
         appBar: AppBar(
           title: Text(categoryTitle),
         ),
         body: ListView.builder(
-          itemBuilder: (ctx, index) {},
-          itemCount: DUMMY_CATEGORIES.length,
+          itemBuilder: (ctx, index) {
+            return Text(categoryMeals[index].title);
+          },
+          itemCount: categoryMeals.length,
         ));
   }
 }
